9d4f40e56876b3a91073c856c4f1685626334a40
Uaciej <amacedmace@gmail.com>
2023-10-02 21:43:08 +0200

    bugfix/ fixed expiration links

--- images/views.py
+++ images/views.py
@@ -12,7 +12,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from io import BytesIO
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class ImageUploadView(APIView):
@@ -21,52 +21,65 @@ class ImageUploadView(APIView):
     def post(self, request, format=None):
         serializer = ImageUploadSerializer(data=request.data)
         if serializer.is_valid():
-            file_ext = os.path.splitext(request.data['file'].name)[1]  # Get the file extension
-            if file_ext.lower() not in ['.jpg', '.jpeg', '.png']:
-                return Response({'detail': 'Invalid file type. Only .jpg, .jpeg, and .png files are allowed.'}, status=status.HTTP_400_BAD_REQUEST)
+            file_ext = os.path.splitext(request.data["file"].name)[
+                1
+            ]  # Get the file extension
+            if file_ext.lower() not in [".jpg", ".jpeg", ".png"]:
+                return Response(
+                    {
+                        "detail": "Invalid file type. Only .jpg, .jpeg, and .png files are allowed."
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             image = serializer.save(user=request.user)
             thumbnail_urls = self.create_thumbnails(image)
             response_data = serializer.data
             response_data.update(thumbnail_urls)
             # Generate expiring link if link is enabled
-            if request.user.tier.expiring_link_enabled: 
-                expiration_time = request.data.get('expiration_time', 300)
+            if request.user.tier.expiring_link_enabled:
+                expiration_time = int(request.data.get("expiration_time", 300))
+                expiration_time = max(300, min(expiration_time, 30000))
                 link = self.generate_expiring_link(image, expiration_time)
-                response_data['expiration_link'] = link
+                response_data["expiration_link"] = link
             # Delete the original file if link not enabled
-            if not request.user.tier.link_enabled: 
-                os.remove(image.file.path)  
-                image.file = None 
-                image.save()  
+            if not request.user.tier.link_enabled:
+                os.remove(image.file.path)
+                image.file = None
+                image.save()
+                del response_data["file"]
             return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def create_thumbnails(self, image):
         user_tier = image.user.tier
-        thumbnail_sizes = user_tier.thumbnail_sizes.split(',')
+        thumbnail_sizes = user_tier.thumbnail_sizes.split(",")
         thumbnail_urls = {}
         for size in thumbnail_sizes:
             url = self.create_thumbnail(image, int(size))
-            thumbnail_urls[f'url_{size}'] = url
+            thumbnail_urls[f"url_{size}"] = url
         return thumbnail_urls
-    
+
     def create_thumbnail(self, image, size):
         img = PILImage.open(image.file.path)
         img.thumbnail((size, size))
 
         # Save the thumbnail to a new file
         thumbnail_io = BytesIO()
-        img_format = img.format  # This will be 'JPEG' for .jpg files and 'PNG' for .png files
+        img_format = (
+            img.format
+        )  # This will be 'JPEG' for .jpg files and 'PNG' for .png files
         img.save(thumbnail_io, format=img_format)
 
         # Create a new Image instance and save the file to it
         thumbnail = Image(user=image.user)
-        filename_base, filename_ext = os.path.splitext(os.path.basename(image.file.name))
-        file_name = f'{filename_base}_thumbnail_{size}{filename_ext}'
+        filename_base, filename_ext = os.path.splitext(
+            os.path.basename(image.file.name)
+        )
+        file_name = f"{filename_base}_thumbnail_{size}{filename_ext}"
         thumbnail.file.save(file_name, ContentFile(thumbnail_io.getvalue()), save=False)
         thumbnail.save()
         return thumbnail.file.url
-    
+
     def generate_expiring_link(self, image, expiration_time):
         # Create a signer
         signer = signing.TimestampSigner()
@@ -76,17 +89,20 @@ class ImageUploadView(APIView):
         signed_value = signer.sign(value)
 
         # Generate the URL for the view that will handle the signed link
-        link = reverse('signed_image_view', args=[signed_value])
+        link = reverse("signed_image_view", args=[signed_value])
 
         return link
-        
 
 
 class UserImagesList(generics.ListAPIView):
     serializer_class = ImageSerializer
 
     def get_queryset(self):
-        return Image.objects.filter(user=self.request.user).exclude(file__isnull=True).exclude(file='')
+        return (
+            Image.objects.filter(user=self.request.user)
+            .exclude(file__isnull=True)
+            .exclude(file="")
+        )
 
 
 def signed_image_view(request, signed_value):
@@ -103,7 +119,8 @@ def signed_image_view(request, signed_value):
     image_id, expiration_time = value.split(":")
 
     # Check if the link has expired
-    if datetime.now() > datetime.fromtimestamp(int(expiration_time)):
+    expiration_time = datetime.now() + timedelta(seconds=int(expiration_time))
+    if datetime.now() > expiration_time:
         raise Http404("Expired link")
 
     # Get the image
@@ -111,11 +128,11 @@ def signed_image_view(request, signed_value):
 
     # Determine the content type based on the file extension
     file_ext = os.path.splitext(image.file.name)[1].lower()
-    if file_ext in ['.jpg', '.jpeg']:
-        content_type = 'image/jpeg'
-    elif file_ext == '.png':
-        content_type = 'image/png'
+    if file_ext in [".jpg", ".jpeg"]:
+        content_type = "image/jpeg"
+    elif file_ext == ".png":
+        content_type = "image/png"
 
     # Open the image file and return it as a response
-    with open(image.file.path, 'rb') as img:
+    with open(image.file.path, "rb") as img:
         return HttpResponse(img.read(), content_type=content_type)
